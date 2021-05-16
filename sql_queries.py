52da4013c77af1ffbb3a25c6726350b97178a6bf
alice <bow1226@gmail.com>
2021-05-16 09:43:49 +0800

    style: Format

--- sql_queries.py
+++ sql_queries.py
@@ -78,7 +78,20 @@ user_agent)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """)
 
-user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) ON CONFLICT (user_id) DO UPDATE SET first_name = EXCLUDED.first_name, last_name = EXCLUDED.last_name, gender = EXCLUDED.gender, level = EXCLUDED.level""")
+user_table_insert = ("""
+INSERT INTO users (
+user_id,
+first_name,
+last_name,
+gender,
+level)
+VALUES (%s, %s, %s, %s, %s) 
+ON CONFLICT (user_id) 
+DO UPDATE SET first_name = EXCLUDED.first_name,
+last_name = EXCLUDED.last_name,
+gender = EXCLUDED.gender,
+level = EXCLUDED.level
+""")
 
 song_table_insert = ("""
 INSERT INTO songs (
